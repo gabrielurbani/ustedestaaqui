91ddaec9a667e902ee01b8fbee99816ef82df728
Gabriel Urbani <gabrielurbani@gmail.com>
2020-11-08 11:02:37 -0300

    Create app.js
    subo app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let myMap;
+let canvas;
+const MAPPA = new Mappa('Leaflet');
+const COORDENADAS = {
+    lat: -34.599722,
+    lng: -58.381944,
+    zoom: 11.5,
+    style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+}
+
+function setup() {
+    canvas = createCanvas(800, 640);
+    myMap = MAPPA.tileMap(COORDENADAS);
+    myMap.overlay(canvas);
+
+}
+
+function draw() {
+
+}
